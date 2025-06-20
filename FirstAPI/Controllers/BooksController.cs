469a202f042b3d018222baa92cf7052eea2de7e3
Nizaam Cole <nizaamcoleyunus18@gmail.com>
2025-06-20 17:38:41 +0200

    Removed commands

--- FirstAPI/Controllers/BooksController.cs
+++ FirstAPI/Controllers/BooksController.cs
@@ -11,44 +11,6 @@ namespace FirstAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        //static private List<Book> books = new List<Book>
-        //{
-        //    new Book
-        //    {
-        //        Id = 1,
-        //        Title = "The Great Gatsby",
-        //        Author = "F. Scott Fitzgerald",
-        //        YearPublished = 1925
-        //    },
-        //    new Book
-        //    {
-        //        Id = 2,
-        //        Title = "To Kill a Mockingbird",
-        //        Author = "Harper Lee",
-        //        YearPublished = 1960
-        //    },
-        //    new Book
-        //    {
-        //        Id = 3,
-        //        Title = "1984",
-        //        Author = "George Orwell",
-        //        YearPublished = 1949
-        //    },
-        //    new Book
-        //    {
-        //        Id = 4,
-        //        Title = "Pride and Prejudice",
-        //        Author = "Jane Austen",
-        //        YearPublished = 1813
-        //    },
-        //    new Book
-        //    {
-        //        Id = 5,
-        //        Title = "Moby-Dick",
-        //        Author = "Herman Melville",
-        //        YearPublished = 1851
-        //    },
-        //};
         private readonly FirstAPIContext _context;
         public BooksController(FirstAPIContext context)
         {
